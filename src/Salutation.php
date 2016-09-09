f5e557dd7d86b06775d68a87190d50f9d9bccca0
Thomas Praxl <thomas@macrominds.de>
2016-09-09 15:29:00 +0200

    Add a simple enum type and test basic features
    Needs to be refactored to a trait or baseclass, so that the user can easily create and use enums.

---
+++ src/Salutation.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Thomas Praxl <thomas@macrominds.de>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace macrominds\enums;
+
+class Salutation
+{
+    private $value = null;
+
+    public static $map;
+
+    protected function __construct($value) {
+        $this->value = $value;
+    }
+
+    public static function init () {
+        self::$map = [
+			'MRS' => new static(1),
+		    'MR' => new static(2),
+		    'MS' => new static(3)
+    	];
+    }
+
+    public static function __callStatic($element, $arguments) {
+        if(isset(self::$map[$element])){
+        	return self::$map[$element];
+        }
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined Enum value '.$element.' for ' . static::class .
+            ' in ' . $trace[0]['file'] .
+            ' line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
+    }
+
+    public function value() {
+        return $this->value;
+    }
+
+    public function __toString () {
+        return ''.$this->value;
+    }
+}
+Salutation::init();
---
+++ tests/EnumTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Thomas Praxl <thomas@macrominds.de>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace macrominds\tests;
+use macrominds\enums\Salutation;
+
+class EnumTest extends \PHPUnit_Framework_TestCase
+{
+	 /** @test */
+	 public function it_should_provide_instances_of_the_enum(){
+	 	$this->assertNotNull(Salutation::MR());
+	 	$this->assertInstanceOf(Salutation::class, Salutation::MR());
+
+	 	// same instance
+	 	$this->assertEquals(Salutation::MR(),Salutation::MR());
+	 	// just to be sure:
+	 	$this->assertTrue(Salutation::MR()===Salutation::MR());
+	 	// not same instance
+	 	$this->assertNotEquals(Salutation::MR(), Salutation::MRS());
+	 	// just to be sure:
+	 	$this->assertFalse(Salutation::MR()===Salutation::MRS());
+	 }
+
+	 /** @test */
+	 public function it_should_provide_a_value(){
+	 	$this->assertEquals(2, Salutation::MR()->value());
+	 	$this->assertEquals(2, ''.Salutation::MR());
+	 }
+
+	 /** @test */
+	 public function it_cannot_be_instanciated_directly(){
+	 	$this->expectException(\Error::class);
+	 	new Salutation(4);
+	 }
+}
